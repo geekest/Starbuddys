57ca2b8168d6aa59a0aa5d52be1fbd8ea51f8989
Harvey Wang <xjwwhw@gmail.com>
2026-05-13 10:01:52 +0800

    新建Starbuddys Xcode工程文件

---
+++ Starbuddys/Starbuddys/StarbuddysApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarbuddysApp.swift
+//  Starbuddys
+//
+//  Created by Geekest on 2026/5/13.
+//
+
+import SwiftUI
+
+@main
+struct StarbuddysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Starbuddys/StarbuddysTests/StarbuddysTests.swift
@@ -0,0 +1,16 @@
+//
+//  StarbuddysTests.swift
+//  StarbuddysTests
+//
+//  Created by Geekest on 2026/5/13.
+//
+
+import Testing
+
+struct StarbuddysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
